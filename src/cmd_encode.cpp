4f8aad9102643e5df292784ce7afead44054649c
Doug Hoyte <doug@hcsw.org>
2024-06-14 12:04:31 -0400

    np

--- src/cmd_encode.cpp
+++ src/cmd_encode.cpp
@@ -3,6 +3,8 @@
 #include <docopt.h>
 #include "golpe.h"
 
+#include "pcg_random.hpp"
+
 
 static const char USAGE[] =
 R"(
@@ -10,6 +12,12 @@ R"(
       encode
 )";
 
+
+
+struct IndexGenerator {
+    uint8_t hash[16];
+    uint64_t prev;
+};
 
 
 struct CodedSymbol {
@@ -40,9 +48,16 @@ struct Decoder {
 void cmd_encode(const std::vector<std::string> &subArgs) {
     std::map<std::string, docopt::value> args = docopt::docopt(USAGE, subArgs, true, "");
 
+    uint64_t seed[2] = {1,3};
+    pcg64 rng(seed[0], seed[1]);
+
+    LW << rng();
+
+/*
     std::string line;
 
     while (std::cin) {
         std::getline(std::cin, line);
     }
+    */
 }
