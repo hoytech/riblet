c70db6157e6b8723419eea1fdaec02305c2270c0
Doug Hoyte <doug@hcsw.org>
2024-06-14 09:55:04 -0400

    np

--- src/cmd_encode.cpp
+++ src/cmd_encode.cpp
@@ -11,6 +11,32 @@ R"(
 )";
 
 
+
+struct CodedSymbol {
+    std::string valAccum;
+    uint8_t hashAccum[16] = {0};
+    uint64_t count = 0;
+};
+
+struct Encoder {
+    std::vector<CodedSymbol> codedSymbols;
+
+    void allocate(uint64_t n) {
+        codedSymbols.resize(n);
+    }
+
+    void add(std::string_view val) {
+        // Hash
+        // Make PGC64 instance
+        // Iterate PRNG until index > codedSymbols
+        //   append onto codedSymbols
+    }
+};
+
+struct Decoder {
+};
+
+
 void cmd_encode(const std::vector<std::string> &subArgs) {
     std::map<std::string, docopt::value> args = docopt::docopt(USAGE, subArgs, true, "");
 
